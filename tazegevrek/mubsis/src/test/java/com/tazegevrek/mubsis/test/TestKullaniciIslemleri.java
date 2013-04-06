becf009a6499541b9183b23c37a56388fdc6654f
er3n <erenozturk@gmail.com>
2013-04-06 20:25:19 +0300

    Activiti unit test yazıldı.

--- tazegevrek/mubsis/src/test/java/com/tazegevrek/mubsis/test/TestKullaniciIslemleri.java
+++ tazegevrek/mubsis/src/test/java/com/tazegevrek/mubsis/test/TestKullaniciIslemleri.java
@@ -2,6 +2,8 @@ package com.tazegevrek.mubsis.test;
 
 import junit.framework.Assert;
 
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,18 +15,33 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tazegevrek.mubsis.domain.dto.NewUserDTO;
 import com.tazegevrek.mubsis.domain.entity.Kullanici;
+import com.tazegevrek.mubsis.service.activiti.ConfirmationService;
 import com.tazegevrek.mubsis.service.dao.KullaniciDao;
+import com.tazegevrek.mubsis.service.kullanici.RegistrationConfirmation;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:appcontext/mubsis-test-config.xml"})
 @Component
-public class TestKullaniciIslemleri {
+public class TestKullaniciIslemleri extends AbstractTest {
 
 	private Logger logger = Logger.getLogger(TestKullaniciIslemleri.class);
 	
 	@Autowired
 	private KullaniciDao kullaniciDao;
+	
+	@Autowired
+	private ConfirmationService confirmationService;
+
+	@Autowired
+	private RegistrationConfirmation registrationConfirmation;
+	
+	@Autowired
+	private Md5PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private RuntimeService runtimeService;
 	
 	
 	@Test
@@ -37,8 +54,34 @@ public class TestKullaniciIslemleri {
 	@Test
 	@Transactional(propagation=Propagation.SUPPORTS,readOnly=false)
 	public void passwordCreator(){
-		Md5PasswordEncoder e = new Md5PasswordEncoder();
-		System.out.println(e.encodePassword("123456", null));
+		System.out.println(passwordEncoder.encodePassword("123456", null));
+	}
+	
+	@Test
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=false)
+	public void userRegistrationTest(){
+		
+		NewUserDTO newUser = new NewUserDTO();
+		newUser.setEmail("dummy1@gmail.com");
+		newUser.setGsmNo(5053367649l);
+		newUser.setName("Test");
+		newUser.setPassword(passwordEncoder.encodePassword("123456", null));
+		newUser.setName("Unit");
+		newUser.setSurname("Test");
+		
+		String bussinessKey = confirmationService
+				.startAsycConfirmation(registrationConfirmation, newUser);
+		
+		ProcessInstance  processInstance = runtimeService.createProcessInstanceQuery().processInstanceBusinessKey(bussinessKey).singleResult();
+		
+		Assert.assertNotNull(processInstance);
+		
+		confirmationService.confirmAsycConfirmation(bussinessKey);
+		
+		Kullanici kullanici = kullaniciDao.kullaniciSorgula(newUser.getEmail());
+		
+		Assert.assertNotNull(kullanici);
+		
 	}
 	
 
